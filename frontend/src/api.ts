102c6d554cb406c0f2a138ad506a8183c8941743
thor-tor <michal.kabara@gmail.com>
2025-04-30 20:36:26 +0200

    fix: moves sumbission function do seperate api file, moves api url to env file, adds useFormStatus hook to control form status

---
+++ frontend/src/api.ts
@@ -0,0 +1,29 @@
+import { FormData } from './components/Form/Form'
+
+export const handleSubmitForm = async (e: React.FormEvent<HTMLFormElement>, formData: FormData) => {
+  e.preventDefault()
+
+  if (formData.name === '' || formData.age === '' || formData.favoriteColor === '') {
+    return
+  }
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API}//survey/submit`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message)
+    } else {
+      console.error('An unknown error occurred')
+    }
+  }
+}
