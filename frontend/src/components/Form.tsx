3bad2e7bf55dde5cafc219c4e5b5a0d2a668d7f7
thor-tor <michal.kabara@gmail.com>
2025-04-30 11:43:33 +0200

    feat:adds form submission function to form, moves form to separate component, fix to some styles, adds required prop to FormField

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import { FormField } from './FormField'
+
+export interface FormData {
+  name: string
+  age: string
+  favoriteColor: string
+  preferredContactMethod: string
+}
+
+export const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    favoriteColor: '',
+    preferredContactMethod: 'Email',
+  })
+  const contactMethods = ['Email', 'Phone']
+
+  const handleSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (formData.name === '' || formData.age === '' || formData.favoriteColor === '') {
+      return
+    }
+
+    try {
+      const response = await fetch('http://gateway:8080/api/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message)
+      } else {
+        console.error('An unknown error occurred')
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1>Formul.ai</h1>
+      <form className="flex flex-col gap-4 md:w-3/5 w-full" onSubmit={handleSubmitForm}>
+        <FormField
+          formData={formData}
+          setFormData={setFormData}
+          type="text"
+          label="Name"
+          name="name"
+          required={true}
+        />
+        <FormField
+          formData={formData}
+          setFormData={setFormData}
+          type="text"
+          label="Age"
+          name="age"
+          required={true}
+        />
+        <FormField
+          formData={formData}
+          setFormData={setFormData}
+          type="text"
+          label="Fav Color"
+          name="favoriteColor"
+          required={true}
+        />
+        <div className="flex flex-col gap-2 items-start">
+          <label htmlFor="preferredContactMethod">Preferred Contact Method</label>
+          <select
+            className="border border-zinc-300 rounded-md p-2 w-full"
+            name="preferredContactMethod"
+            id="preferredContactMethod"
+            onChange={(e) => setFormData({ ...formData, preferredContactMethod: e.target.value })}
+          >
+            {contactMethods.map((method) => (
+              <option key={method} value={method}>
+                {method}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="bg-blue-500 text-white py-2 rounded-xl cursor-pointer">Submit</button>
+      </form>
+    </div>
+  )
+}
