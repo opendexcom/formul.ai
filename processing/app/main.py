83680f30718b8de5f3299698a54d19cff4a8f7f7
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-01 15:12:14 +0200

    processing/: refactor construct_prompt

--- processing/app/main.py
+++ processing/app/main.py
@@ -16,25 +16,32 @@ def create_item(item: Item):
     return {"item_received": item}
 
 
-def construct_propmpt(request:ProcessSurveyRequest) -> str:
-    prompt = "<input>\n"
-    prompt += f"You will be provided with survey responses in `answers` tag, one user answer is in `answer` tag. Question for survey: {request.question}\n"
-    prompt += "</input>\n"
-    prompt += "\n"
-    prompt += "<instruction>\n"
-    prompt += "Rewiew and analize responses from `answers` with goal in mind to find and extract informations that will be relevant for question. Treat similar points like one. Prefix each point accounted in final response with: \"frequent\", \"moderate\", \"occasional\" depending how often it was mentioned.\n"
-    prompt += "</instruction>\n"
-    prompt += "\n"
-    prompt += "<answers>\n"
-    for answer in request.answers:
-        prompt += f"<answer>{answer}</answer>\n"
-    prompt += "</answers>"
+def construct_prompt(request:ProcessSurveyRequest) -> str:
+    answer_tags = [f"<answer>{answer}</answer>" for answer in request.answers]
+    answers_xml = "\n".join(answer_tags)
+
+    prompt = f"""
+<input>
+You will be provided with survey responses in `answers` tag, one user answer is in `answer` tag. Question for survey: {request.question}
+</input>
+
+<instruction>
+Rewiew and analize responses from `answers` with goal in mind to find and extract informations that will be relevant for question. Treat similar points like one. Prefix each point accounted in final response with: \"frequent\", \"moderate\", \"occasional\" depending how often it was mentioned.
+</instruction>
+
+<answers>
+{answers_xml}
+</answers>
+    """
+
+    print(prompt)
+
     return prompt
 
 
 async def ask_ollama_process(request: ProcessSurveyRequest,client: AsyncClient) -> str:
 
-    prompt = construct_propmpt(request)
+    prompt = construct_prompt(request)
 
     res=await client.chat(
         model="mistral:latest",
