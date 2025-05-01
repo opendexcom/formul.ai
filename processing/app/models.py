6cc71e29cd9fb39b48bd725cd90c62aea471aa76
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-01 22:23:46 +0200

    processing/: change created_at default_factory to timezone aware one

--- processing/app/models.py
+++ processing/app/models.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 from enum import StrEnum
@@ -19,6 +19,6 @@ class StatusEnum(StrEnum):
 class AnalysisJob(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     survey_id: str = Field(index=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     status: StatusEnum = Field(default=StatusEnum.NULL)
 
