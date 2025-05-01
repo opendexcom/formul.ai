6aab9435784aec84e2555830c34b13ddab7f6710
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-01 23:50:10 +0200

    processing/: remove dropping  from create_db_and_tables

--- processing/app/database.py
+++ processing/app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import Engine
 from sqlmodel import SQLModel
 
 def create_db_and_tables(engine: Engine):
-    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
 
 def reset_db(engine: Engine):
