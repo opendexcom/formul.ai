16b05e48ee2a6fa14d5f8829b5698b4c09d6749a
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-02 02:46:28 +0200

    processing/: specify UUID version 4 for AnalysisJob.id

--- processing/app/models.py
+++ processing/app/models.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, timezone
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 from sqlmodel import Field, SQLModel
 from enum import StrEnum
 
@@ -16,7 +16,7 @@ class StatusEnum(StrEnum):
     ERROR = "error"
 
 class AnalysisJob(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: UUID4 = Field(default_factory=uuid.uuid4, primary_key=True)
     survey_id: str = Field(index=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     status: StatusEnum = Field(default=StatusEnum.NULL)
