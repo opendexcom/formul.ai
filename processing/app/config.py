0624e9ba69424ec4b03be28b2dd7451f03386e5e
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-02 02:42:53 +0200

    processing/: remove SURVEY_ form connection env

--- processing/app/config.py
+++ processing/app/config.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class DatabaseSettings(BaseSettings):
-    db_connection_url:str = Field(default="",alias="SURVEY_DB_CONNECTION_STRING")
+    db_connection_url:str = Field(default="",alias="DB_CONNECTION_STRING")
 
 class Settings(BaseSettings):
     database: DatabaseSettings = DatabaseSettings()
