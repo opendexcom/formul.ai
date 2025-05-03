53418408095f42d1ab057b964691baea3b005ccf
thor-tor <michal.kabara@gmail.com>
2025-05-04 00:02:48 +0200

    fix:adds working api connection with single survey

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,12 +1,14 @@
 import { FormData } from './components/Form/SignupForm'
 
+const ID = '23e4693c-3975-4d91-a2f2-190993043c1c'
+
 export const submitForm = async (formData: FormData) => {
-  const response = await fetch(`${import.meta.env.VITE_API}/survey/submit`, {
+  const response = await fetch(`${import.meta.env.VITE_API}/api/survey/v1/surveys/${ID}/submit`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify({ surveyId: ID, answersJson: JSON.stringify(formData) }),
   })
 
   if (!response.ok) {
