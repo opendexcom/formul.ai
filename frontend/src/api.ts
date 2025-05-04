bef86e1b3d956bbe94134454df056effb00323ec
Michał Kabara <56455625+michalkabara@users.noreply.github.com>
2025-05-04 21:16:11 +0200

    fix:adds working api connection with single survey (#53)
    * fix:adds working api connection with single survey
    
    * fix: implements api url using URL constructor, fix to gitignore to not ignore .env.development.local file

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,12 +1,18 @@
 import { FormData } from './components/Form/SignupForm'
 
+const surveyId = '23e4693c-3975-4d91-a2f2-190993043c1c'
+
 export const submitForm = async (formData: FormData) => {
-  const response = await fetch(`${import.meta.env.VITE_API}/survey/submit`, {
+  const url = new URL(
+    `/api/survey/v1/surveys/${surveyId}/submit`,
+    import.meta.env.VITE_API,
+  ).toString()
+  const response = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify({ surveyId: surveyId, answersJson: JSON.stringify(formData) }),
   })
 
   if (!response.ok) {
