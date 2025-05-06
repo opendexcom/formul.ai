8ab82fe65876f13a9b5ca1360bae720b8ceeefd1
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-06 21:56:29 +0200

    fix: creating and dropping only own tables

--- processing/app/database.py
+++ processing/app/database.py
@@ -1,23 +1,34 @@
 import sys
+from functools import lru_cache
 
+from app.models import Task
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlmodel import SQLModel
+from sqlmodel import Table
+
+owned_table_names = [Task.__tablename__]
+
+
+@lru_cache()
+def get_owned_tables() -> list[Table]:
+    owned_tables: list[Table] = []
+    tables = SQLModel.metadata.sorted_tables
+    for table in tables:
+        print(f"Table: {table.name}")
+        if table.name in owned_table_names:
+            owned_tables.append(table)
+    print(f"owned_tables: {owned_tables}")
+    return owned_tables
 
 
 async def create_db_and_tables(engine: AsyncEngine):
     async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+        tables_to_create = get_owned_tables()
+        await conn.run_sync(SQLModel.metadata.create_all, tables=tables_to_create)
 
 
 async def reset_db(engine: AsyncEngine):
     async with engine.begin() as conn:
-        tables = SQLModel.metadata.sorted_tables
-        tables_to_drop = []
-        for table in tables:
-            if table.name.startswith("processing_"):
-                tables_to_drop.append(table)
-        print(f"Tables: {tables}")
-        print(f"Tables to drop: {tables_to_drop}")
-        sys.stdout.flush()
-        await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
+        tables_to_drop = get_owned_tables()
+        await conn.run_sync(SQLModel.metadata.drop_all, tables=tables_to_drop)
+        await conn.run_sync(SQLModel.metadata.create_all, tables=tables_to_drop)
