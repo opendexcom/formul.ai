581c1b7a27e4081bde51a36929ee79611c8899d9
ZgredFred <fryderyksucharczuk@gmail.com>
2025-05-06 13:30:43 +0200

    fix: adapt /processing/task/{id}/status test to async engine

--- processing/app/tests/test_processing.py
+++ processing/app/tests/test_processing.py
@@ -1,33 +1,43 @@
 from uuid import uuid4
 
+from app.core.exceptions import NotFoundError
 from app.deps import get_task_service
 from app.main import app
+from app.models import AnalysisTask
 from app.models import AnalysisTaskResult
 from app.models import AnalysisTaskStatus
 from fastapi.testclient import TestClient
+from pydantic import UUID4
 from sqlmodel import UUID
 
 client = TestClient(app)
 
 
 def test_get_task_status():
-    task_id = uuid4()
+    local_task_id = uuid4()
 
     def get_task_service_mock():
         class MockTaskService:
-            def get_task_status_by_id(self, task_id):
-                return AnalysisTaskStatus.NULL
+            async def get_task_by_id(self, task_id: UUID4) -> AnalysisTask:
+                if task_id != local_task_id:
+                    raise NotFoundError(f"Task with ID {task_id} not found")
+                return AnalysisTask(
+                    id=task_id,
+                    survey_id=uuid4(),
+                    status=AnalysisTaskStatus.NULL,
+                    result=None,
+                )
 
         return MockTaskService()
 
     app.dependency_overrides[get_task_service] = get_task_service_mock
 
-    response = client.get(f"/processing/task/{task_id}/status")
+    response = client.get(f"/processing/task/{local_task_id}/status")
 
     assert response.status_code == 200
     assert response.json() == {
         "status": AnalysisTaskStatus.NULL.value,
-        "task_id": str(task_id),
+        "task_id": str(local_task_id),
     }
 
 
@@ -37,10 +47,10 @@ def test_get_completed_task_file():
     local_task_result = '{"ok":"true"}'
 
     class MockTaskService:
-        def get_task_result_by_id(self, task_id: UUID) -> AnalysisTaskResult:
+        async def get_task_by_id(self, task_id: UUID) -> AnalysisTask:
             if task_id != local_task_id:
                 raise ValueError(f"Task with ID {task_id} not found")
-            return AnalysisTaskResult(
+            return AnalysisTask(
                 result=local_task_result,
                 survey_id=local_survey_id,
                 status=AnalysisTaskStatus.COMPLETED,
@@ -64,10 +74,10 @@ def test_get_non_completed_task_file():
     local_survey_id = uuid4()
 
     class MockTaskService:
-        def get_task_result_by_id(self, task_id: UUID) -> AnalysisTaskResult:
+        async def get_task_by_id(self, task_id: UUID) -> AnalysisTask:
             if task_id != local_task_id:
                 raise ValueError(f"Task with ID {task_id} not found")
-            return AnalysisTaskResult(
+            return AnalysisTask(
                 result=None,
                 survey_id=local_survey_id,
                 status=AnalysisTaskStatus.NULL,
