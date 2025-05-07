6b456bcf415c3d9822b31931ee9e056a51fbd232
Veresek <filipowskipraca@gmail.com>
2025-05-08 01:40:29 +0200

    add get request for surveys

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
-import ThankYouPage from './ThankYouPage.tsx'
+import App from './pages/App.tsx'
+import ThankYouPage from './pages/ThankYouPage.tsx'
+import SurveysPage from './pages/SurveysPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
+        <Route path="/surveys" element={<SurveysPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
