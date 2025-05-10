6b64da89f45ceddf4b025385a2b8cd3538855db7
Ban <bartek.krzys+github@gmail.com>
2025-05-10 13:30:17 +0200

    fix: update seed SQL to resolve parsing error at execution time

--- survey/src/main/resources/data.sql
+++ survey/src/main/resources/data.sql
@@ -4,7 +4,7 @@
 
 -- DROP TABLE public.survey;
 
-CREATE TABLE if not exists public.survey (
+CREATE TABLE IF NOT EXISTS public.survey (
 	id uuid NOT NULL,
 	"name" varchar(255) NULL,
 	schema_json text NULL,
@@ -18,24 +18,13 @@ CREATE TABLE if not exists public.survey (
 -- DROP TABLE public.survey_answers;
 
 
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.tables 
-        WHERE table_schema = 'public'
-        AND table_name = 'survey_answers'
-    ) THEN
-        CREATE TABLE public.survey_answers (
-	id uuid NOT NULL,
-	answers_json varchar(255) NULL,
-	survey_id uuid NULL,
-	CONSTRAINT survey_answers_pkey PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS public.survey_answers (
+    id uuid NOT NULL,
+    answers_json varchar(255) NULL,
+    survey_id uuid NULL,
+    CONSTRAINT survey_answers_pkey PRIMARY KEY (id),
+    CONSTRAINT survey__survey_answers_fkey FOREIGN KEY (survey_id) REFERENCES public.survey(id)
 );
-ALTER TABLE public.survey_answers ADD CONSTRAINT fkp02l4sqlckil64wrg194t9dbj FOREIGN KEY (survey_id) REFERENCES public.survey(id);
-
-		    END IF;
-END $$;
 
 
 INSERT INTO survey(id, name, schema_json)
