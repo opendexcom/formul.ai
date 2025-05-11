b35f77491c729f95b04ac22f51e279b2638d86d8
Veresek <filipowskipraca@gmail.com>
2025-05-12 00:46:33 +0200

    feat:  add custom theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,8 @@ import ThankYouPage from './pages/ThankYou.page.tsx'
 import SurveysPage from './pages/Surveys.page.tsx'
 import Error404Page from './pages/Error404.page.tsx'
 import AppContainer from './components/AppContainer.tsx'
-import { GlobalStyles } from '@mui/material'
+import theme from './theme/index.ts'
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -20,16 +21,19 @@ createRoot(document.getElementById('root')!).render(
           },
         }}
       />
-      <AppContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/form/:id" element={<FormPage />} />
-            <Route path="/thank-you" element={<ThankYouPage />} />
-            <Route path="/surveys" element={<SurveysPage />} />
-            <Route path="*" element={<Error404Page />} />
-          </Routes>
-        </BrowserRouter>
-      </AppContainer>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppContainer>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/form/:id" element={<FormPage />} />
+              <Route path="/thank-you" element={<ThankYouPage />} />
+              <Route path="/surveys" element={<SurveysPage />} />
+              <Route path="*" element={<Error404Page />} />
+            </Routes>
+          </BrowserRouter>
+        </AppContainer>
+      </ThemeProvider>
     </>
   </StrictMode>,
 )
