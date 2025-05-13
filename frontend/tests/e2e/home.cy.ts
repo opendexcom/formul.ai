ae43d735889ab9fe685c76285dfce2279a4528a0
thor-tor <michal.kabara@gmail.com>
2025-05-13 20:46:30 +0200

    fix: adds port to url of frontend

--- frontend/tests/e2e/home.cy.ts
+++ frontend/tests/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe('Homepage', () => {
   it('loads correctly', () => {
-    cy.visit('http://localhost')
+    cy.visit('http://localhost:5173')
     cy.contains('Welcome').should('be.visible')
   })
 })
