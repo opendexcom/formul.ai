e691c73a535023d975189484f9bc2483d4562f9b
thor-tor <michal.kabara@gmail.com>
2025-05-22 12:35:07 +0200

    chore: adds snackbar and error handling for better ux

--- frontend/src/pages/SurveyEditor.page.tsx
+++ frontend/src/pages/SurveyEditor.page.tsx
@@ -1,5 +1,16 @@
 import { Layout } from '@/features/shared'
-import { Button, Card, CardContent, Grid2, TextField } from '@mui/material'
+import {
+  Alert,
+  AlertColor,
+  Button,
+  Card,
+  CardContent,
+  Grid2,
+  Snackbar,
+  SnackbarCloseReason,
+  TextField,
+  Typography,
+} from '@mui/material'
 import { withTheme } from '@rjsf/core'
 import { Theme } from '@rjsf/mui'
 import validator from '@rjsf/validator-ajv8'
@@ -30,30 +41,80 @@ export const SurveyEditor = () => {
       },
     },
   })
+  const [loading, setLoading] = useState(false)
+  const [jsonError, setJsonError] = useState(false)
+  const [schemaChanged, setSchemaChanged] = useState(false)
+  const [snackBarData, setSnackBarData] = useState<{
+    open: boolean
+    variant: AlertColor
+    message: string
+  }>({ open: false, variant: 'info', message: '' })
 
   const handleSaveSurvey = async () => {
+    if (jsonError) {
+      setSnackBarData({ open: true, variant: 'error', message: 'Fix JSON schema and try again' })
+      return
+    }
+
+    setLoading(true)
     try {
       await addNewForm(newFormSchema.title ?? '', JSON.stringify(newFormSchema))
-      navigate('/surveys')
+      navigate('/surveys', {
+        state: {
+          snackbar: { message: `Added new survey - ${newFormSchema.title}`, severity: 'success' },
+        },
+      })
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message)
+        setSnackBarData({ open: true, variant: 'error', message: error.message })
       } else {
         console.error('An unknown error occurred')
       }
     }
   }
 
+  const handleApplySurveyChanges = () => {
+    try {
+      setNewFormSchema(JSON.parse(textFieldRef.current?.value || '{}'))
+      setJsonError(false)
+      setSnackBarData({ open: true, variant: 'info', message: 'JSON data was updated' })
+      setSchemaChanged(false)
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message)
+        setJsonError(true)
+        setSnackBarData({ open: true, variant: 'error', message: error.message })
+      } else {
+        console.error('An unknown error occurred')
+      }
+    }
+  }
+
+  const handleClose = (_event: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setSnackBarData((prev) => ({ ...prev, open: false }))
+  }
+
   return (
     <Layout title="Create Survey">
+      <Snackbar open={snackBarData.open} autoHideDuration={5000} onClose={handleClose}>
+        <Alert onClose={handleClose} variant="filled" severity={snackBarData.variant}>
+          {snackBarData.message}
+        </Alert>
+      </Snackbar>
       <Card>
         <CardContent>
-          <Grid2 container spacing={3}>
-            <Grid2 size={6}>
+          <Grid2 container spacing={3} columns={{ xs: 1, md: 12 }}>
+            <Grid2 size={{ xs: 1, md: 6 }}>
               <Card>
                 <CardContent>
                   <Grid2 container spacing={2}>
                     <TextField
+                      error={jsonError}
                       inputRef={textFieldRef}
                       fullWidth
                       multiline
@@ -62,27 +123,33 @@ export const SurveyEditor = () => {
                       label="JSON Schema"
                       variant="outlined"
                       defaultValue={JSON.stringify(newFormSchema, null, '\t')}
+                      onChange={() => setSchemaChanged(true)}
                     />
-                    <Button
-                      onClick={() =>
-                        setNewFormSchema(JSON.parse(textFieldRef.current?.value || '{}'))
-                      }
-                      variant="contained"
-                    >
+                    <Button onClick={handleApplySurveyChanges} variant="contained">
                       Apply changes
                     </Button>
                   </Grid2>
                 </CardContent>
               </Card>
             </Grid2>
-            <Grid2 size={6}>
+            <Grid2 size={{ xs: 1, md: 6 }}>
               <Card>
                 <CardContent>
                   {newFormSchema ? (
                     <>
                       <Form uiSchema={uiSchema} schema={newFormSchema} validator={validator} />
-                      <Button onClick={handleSaveSurvey} variant="contained" color="success">
-                        Save form
+                      {schemaChanged && (
+                        <Typography variant="subtitle2">
+                          Schema was changed. Apply changes and save again.
+                        </Typography>
+                      )}
+                      <Button
+                        disabled={loading || schemaChanged}
+                        onClick={handleSaveSurvey}
+                        variant="contained"
+                        color="success"
+                      >
+                        Save Schema
                       </Button>
                     </>
                   ) : (
