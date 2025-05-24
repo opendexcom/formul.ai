2cc51c29b9bc3c5a41a14e7758e7a9681d1547d4
Rafal Korszun <r.korszun@kleder.co>
2025-05-24 19:37:13 +0200

    refactor: streamline ValidJsonSchemaValidator by removing unused logger and optimizing schema validation

--- survey/src/main/java/com/formulai/survey/validation/ValidJsonSchemaValidator.java
+++ survey/src/main/java/com/formulai/survey/validation/ValidJsonSchemaValidator.java
@@ -7,23 +7,27 @@ import com.networknt.schema.SpecVersion;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ValidJsonSchemaValidator implements ConstraintValidator<ValidJsonSchema, String> {
-    private static final Logger logger = LoggerFactory.getLogger(ValidJsonSchemaValidator.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
 
+    /**
+     * Validates whether the provided string is a valid JSON schema.
+     *
+     * @param value   the JSON schema as a string to validate
+     * @param context the context in which the constraint is evaluated
+     * @return {@code true} if the input string is a valid JSON schema, {@code false} otherwise
+     */
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isEmpty()) return false;
         try {
             JsonNode schemaNode = objectMapper.readTree(value);
-            JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
-            factory.getSchema(schemaNode);
+            jsonSchemaFactory .getSchema(schemaNode);
             return true;
         } catch (Exception e) {
-            logger.warn("Invalid JSON schema: {}", e.getMessage());
+            System.err.println("Invalid JSON schema: " + e.getMessage());
             return false;
         }
     }
